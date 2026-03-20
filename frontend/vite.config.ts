827688b2482cc2686c9f93ce34643004acf6fdc9
Kirillzu86 <kirillzu86@gmail.com>
2026-03-20 15:30:18 +0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // 1. Позволяет Docker пробрасывать порты (Vite слушает не только внутри сети контейнера)
+    host: '0.0.0.0', 
+    port: 5173,
+    // 2. Важно: разрешаем Vite принимать запросы с вашего домена ngrok
+    allowedHosts: [
+      'heartenedly-uncommutable-eda.ngrok-free.dev'
+    ],
+    // 3. Настройка для Hot Module Replacement (чтобы правки в коде сразу отражались в браузере)
+    hmr: {
+      host: 'heartenedly-uncommutable-eda.ngrok-free.dev',
+      clientPort: 443,
+      protocol: 'wss'
+    }
+  }
 })
